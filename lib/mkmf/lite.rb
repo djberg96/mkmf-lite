e9861fc312859a24dd95f00b12e93cfe5c91fc67
Daniel Berger <djberg96@gmail.com>
2010-05-22 22:52:55 -0600

    Added the have_func method, along with corresponding tests and templates.

--- lib/mkmf/lite.rb
+++ lib/mkmf/lite.rb
@@ -8,16 +8,45 @@ module Mkmf
     # The version of the mkmf-lite library
     MKMF_LITE_VERSION = '0.1.0'
 
-    @@cpp_command = Config::CONFIG['CC'] || Config::CONFIG['CPP']
-    @@cpp_outfile = Config::CONFIG['CPPOUTFILE'] # -o conftest.i
-    @@cpp_srcfile = 'conftest.c'
-    
+    @@cpp_command   = Config::CONFIG['CC'] || Config::CONFIG['CPP']
+    @@cpp_outfile   = Config::CONFIG['CPPOUTFILE'] # -o conftest.i
+    @@cpp_libraries = Config::CONFIG['LIBS'] + Config::CONFIG['LIBRUBYARG']
+    @@cpp_srcfile   = 'conftest.c'
+
     # Check for the presence of the given +header+ file.
+    #
+    # Returns true if found, or false if not found.
     #
     def have_header(header)
       erb  = ERB.new(read_template('have_header.erb'))
       result = erb.result(binding)
       try_to_compile(result)
+    end
+
+    # Check for the presence of the given +function+ in the common header
+    # files, or within any +headers+ that you provide.
+    #
+    # Returns true if found, or false if not found.
+    #
+    def have_func(function, headers = [])
+      headers = [headers] unless headers.is_a?(Array)
+
+      unless Config::CONFIG['COMMON_HEADERS'].empty?
+        headers += Config::CONFIG['COMMON_HEADERS']
+      end
+
+      headers  = headers.flatten.uniq
+      includes = headers.map{ |h| "#include <#{h}>" }.join("\n")
+
+      erb_ptr = ERB.new(read_template('have_func_pointer.erb'))
+      erb_std = ERB.new(read_template('have_func.erb'))
+
+      pointer  = erb_ptr.result(binding)
+      standard = erb_std.result(binding)
+
+      # Check for just the function pointer first. If that fails, then try
+      # to compile with the function declaration.
+      try_to_compile(pointer) || try_to_compile(standard)
     end
 
     private
@@ -36,7 +65,12 @@ module Mkmf
 
         Dir.chdir(Dir.tmpdir){
           File.open(@@cpp_srcfile, 'w'){ |fh| fh.write(code) }
-          command = @@cpp_command << ' ' << @@cpp_outfile << ' ' << @@cpp_srcfile
+
+          command  = @@cpp_command + ' '
+          command += @@cpp_libraries + ' '
+          command += @@cpp_outfile + ' '
+          command += @@cpp_srcfile
+
           $stderr.reopen(File.null)
           bool = system(command)
         }
--- test/test_mkmf_lite.rb
+++ test/test_mkmf_lite.rb
@@ -23,4 +23,31 @@ class TC_Mkmf_Lite < Test::Unit::TestCase
     assert_true(have_header('stdio.h'))
     assert_false(have_header('foobar.h'))
   end
+
+  test "have_header requires one argument only" do
+    assert_raise(ArgumentError){ have_header }
+    assert_raise(ArgumentError){ have_header('stdio.h', 'stdlib.h') }
+  end
+
+  test "have_func basic functionality" do
+    assert_respond_to(self, :have_func)
+  end
+
+  test "have_func with no arguments returns expected boolean value" do
+    assert_true(have_func('abort'))
+    assert_false(have_header('abortxyz'))
+  end
+
+  test "have_func with arguments returns expected boolean value" do
+    assert_true(have_func('printf', 'stdio.h'))
+    assert_false(have_func('printfx', 'stdio.h'))
+  end
+
+  test "have_func requires at least one argument" do
+    assert_raise(ArgumentError){ have_func }
+  end
+
+  test "have_func accepts a maximum of two arguments" do
+    assert_raise(ArgumentError){ have_func('printf', 'stdio.h', 'bogus') }
+  end
 end
