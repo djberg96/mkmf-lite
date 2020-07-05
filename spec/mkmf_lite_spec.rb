96f13167df74aa51eb4052ba3f9b9590e2075987
Daniel Berger <djberg96@gmail.com>
2020-07-04 21:05:06 -0400

    Finish conversion to rspec.

--- spec/mkmf_lite_spec.rb
+++ spec/mkmf_lite_spec.rb
@@ -41,75 +41,77 @@ describe Mkmf::Lite do
     end
   end
 
-=begin
-  example "have_func basic functionality" do
-    expect(self).to respond_to(:have_func)
+  context "have_func" do
+    example "have_func basic functionality" do
+      expect(subject).to respond_to(:have_func)
+    end
+
+    example "have_func with no arguments returns expected boolean value" do
+      expect(subject.have_func('abort')).to eq(true)
+      expect(subject.have_func('abortxyz')).to eq(false)
+    end
+
+    example "have_func with arguments returns expected boolean value" do
+      expect(subject.have_func('printf', 'stdio.h')).to eq(true)
+      expect(subject.have_func('printfx', 'stdio.h')).to eq(false)
+    end
+
+    example "have_func requires at least one argument" do
+      expect{ subject.have_func }.to raise_error(ArgumentError)
+    end
+
+    example "have_func accepts a maximum of two arguments" do
+      expect{ subject.have_func('printf', 'stdio.h', 'bogus') }.to raise_error(ArgumentError)
+    end
   end
 
-  example "have_func with no arguments returns expected boolean value" do
-    expect(have_func('abort')).to be_true
-    expect(have_func('abortxyz')).to be_false
+  context "have_struct_member" do
+    example "have_struct_member basic functionality" do
+      expect(subject).to respond_to(:have_struct_member)
+    end
+
+    example "have_struct_member returns expected boolean value" do
+      expect(subject.have_struct_member(@st_type, @st_member, @st_header)).to eq(true)
+      expect(subject.have_struct_member(@st_type, 'pw_bogus', @st_header)).to eq(false)
+      expect(subject.have_struct_member(@st_type, @st_member)).to eq(false)
+    end
+
+    example "have_struct_member requires at least two arguments" do
+      expect{ subject.have_struct_member() }.to raise_error(ArgumentError)
+      expect{ subject.have_struct_member('struct passwd') }.to raise_error(ArgumentError)
+    end
+
+    example "have_struct_member accepts a maximum of three arguments" do
+      expect{ subject.have_struct_member('struct passwd', 'pw_name', 'pwd.h', true) }.to raise_error(ArgumentError)
+    end
   end
 
-  example "have_func with arguments returns expected boolean value" do
-    expect(have_func('printf', 'stdio.h')).to be_true
-    expect(have_func('printfx', 'stdio.h')).to be_false
-  end
+  context "check_sizeof" do
+    example "check_sizeof basic functionality" do
+      expect(subject).to respond_to(:check_sizeof)
+      expect{ subject.check_sizeof(@st_type, @st_header) }.not_to raise_error
+    end
 
-  example "have_func requires at least one argument" do
-    expect{ have_func }.to raise_error(ArgumentError)
-  end
+    example "check_sizeof requires at least one argument" do
+      expect{ subject.check_sizeof }.to raise_error(ArgumentError)
+      expect{ subject.check_sizeof('struct passwd', 'pw_name', 1) }.to raise_error(ArgumentError)
+    end
 
-  example "have_func accepts a maximum of two arguments" do
-    expect{ have_func('printf', 'stdio.h', 'bogus') }.to raise_error(ArgumentError)
-  end
+    example "check_sizeof accepts a maximum of two arguments" do
+      expect{ subject.check_sizeof('div_t', 'stdlib.h', 1) }.to raise_error(ArgumentError)
+    end
 
-  example "have_struct_member basic functionality" do
-    expect(self).to respond_to(:have_struct_member)
-  end
+    example "check_sizeof works with one or two arguments" do
+      expect{ subject.check_sizeof('div_t') }.not_to raise_error
+      expect{ subject.check_sizeof('div_t', 'stdlib.h') }.not_to raise_error
+    end
 
-  example "have_struct_member returns expected boolean value" do
-    expect(have_struct_member(@st_type, @st_member, @st_header)).to be_true
-    expect(have_struct_member(@st_type, 'pw_bogus', @st_header)).to be_false
-    expect(have_struct_member(@st_type, @st_member)).to be_false
+    example "check_sizeof returns an integer value" do
+      size = subject.check_sizeof(@st_type, @st_header)
+      expect(size).to be_kind_of(Integer)
+      expect(size).to be > 0
+    end
   end
-
-  example "have_struct_member requires at least two arguments" do
-    expect{ have_struct_member() }.to raise_error(ArgumentError)
-    expect{ have_struct_member('struct passwd') }.to raise_error(ArgumentError)
-  end
-
-  example "have_struct_member accepts a maximum of three arguments" do
-    assert_raise(ArgumentError){
-      have_struct_member('struct passwd', 'pw_name', 'pwd.h', true)
-    }
-  end
-
-  example "check_sizeof basic functionality" do
-    expect(self).to respond_to(:check_sizeof)
-    expect{ check_sizeof(@st_type, @st_header) }.not_to raise_error
-  end
-
-  example "check_sizeof requires at least one argument" do
-    expect{ check_sizeof }.to raise_error(ArgumentError)
-    expect{ check_sizeof('struct passwd', 'pw_name', 1) }.to raise_error(ArgumentError)
-  end
-
-  example "check_sizeof accepts a maximum of two arguments" do
-    expect{ check_sizeof('div_t', 'stdlib.h', 1) }.to raise_error(ArgumentError)
-  end
-
-  example "check_sizeof works with one or two arguments" do
-    expect{ check_sizeof('div_t') }.not_to raise_error
-    expect{ check_sizeof('div_t', 'stdlib.h') }.not_to raise_error
-  end
-
-  example "check_sizeof returns an integer value" do
-    size = check_sizeof(@st_type, @st_header)
-    expect( size).to be_kind_of(Integer)
-    expect(size > 0).to be_true
-  end
-=end
 
   after do
     @st_type   = nil
