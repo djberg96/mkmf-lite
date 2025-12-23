9b35c166ab8497d8b4be0e4c2a887fdb14622946
Daniel Berger <78529+djberg96@users.noreply.github.com>
2025-12-23 04:21:32 -0600

    Move code into template.

--- lib/mkmf/lite.rb
+++ lib/mkmf/lite.rb
@@ -119,14 +119,8 @@ module Mkmf
     #
     def have_library(library, function = nil, headers = [])
       headers = get_header_string(headers)
-
-      # If a function is specified, generate code that references it
-      if function
-        code = "#{headers}\n\nint main(){\n  void *p = (void *)&#{function};\n  return !p;\n}\n"
-      else
-        erb = ERB.new(read_template('have_library.erb'))
-        code = erb.result(binding)
-      end
+      erb = ERB.new(read_template('have_library.erb'))
+      code = erb.result(binding)
 
       # Build link options with the library
       link_options = windows_with_cl_compiler? ? "#{library}.lib" : "-l#{library}"
